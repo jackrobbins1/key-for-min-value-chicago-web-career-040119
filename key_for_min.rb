d7de0b91e18724c95eea2bad8fad1e6452f0745f
Jack Robbins <jackrobbins1@gmail.com>
2019-03-21 18:50:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,8 +6,8 @@ def key_for_min_value(name_hash)
   name_hash.collect do |name, num|
     if mini == {}
       mini = {name: num}
-    elsif mini != {} &&
-
+    elsif mini != {} && mini[:name] < num
+      mini = {name: num}
     end
   end
 end
