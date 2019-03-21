0006586287a1af55795233eda5ace58d095307cb
Jack Robbins <jackrobbins1@gmail.com>
2019-03-21 18:48:34 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,16 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-# def key_for_min_value(name_hash)
-#   mini = {}
-#   name_hash.collect do |name, num|
-#     if mini == {}
-#       mini = name: num
-#     elsif mini != &&
-#
-#     end
-#   end
-# end
+def key_for_min_value(name_hash)
+  mini = {}
+  name_hash.collect do |name, num|
+    if mini == {}
+      mini = name: num
+    elsif mini != &&
 
-Jackson = {name: "Jack"}
-puts Jackson
+    end
+  end
+end
+
+# Jackson = {name: "Jack"}
+# puts Jackson
