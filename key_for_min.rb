38ec8fcb2ba4f3f2333fc7877aa39e44659c4ec8
Jack Robbins <jackrobbins1@gmail.com>
2019-03-21 19:13:44 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,18 +2,35 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  mini = {}
-  the_name = nil
-  name_hash.each do |name, num|
-    if mini == {}
-      mini = Hash[name, num]
-    elsif mini != {} && mini[name] < num
-      mini = Hash[name, num]
-      the_name = name
-    end
-  end
-  the_name
+
 end
 
-# Jackson = {name: "Jack"}
-# puts Jackson
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# mini = {}
+# the_name = nil
+# name_hash.each do |name, num|
+#   if mini == {}
+#     mini = Hash[name, num]
+#   elsif mini != {} && mini[name] < num
+#     mini = Hash[name, num]
+#     the_name = name
+#   end
+# end
+# the_name
